085e05eb94841448dcdb4741796e8440a11b3d72
qihong <qihongbin@163.com>
2015-08-05 17:22:10 +0800

    ff

--- abcd_bower_test.js
+++ abcd_bower_test.js
@@ -1,10 +1,14 @@
-define(function (require, exports, module) {
-
-	var test = {
-		init: function () {
-		
-			console.log(1);
-		}	
-	}
-	module.exports = test;
-});
+define("widgets/qihong-test-ll/abcd_bower_test", [], function(require, exports, module) {
+    function Widget1() {
+	        if (!(this instanceof Widget1)) {
+			            return new Widget1();
+						        }
+								        this.name = "Widget1";
+										    }
+											    Widget1.prototype = {
+												        init: function() {
+														            console.log(this.name);
+																	        }
+																			    };
+																				    module.exports = Widget1;
+																					});
